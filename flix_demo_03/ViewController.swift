24b3a2163fd0225919f189ae23d4d813da5599a7
Joseantonio Maciel <joseantonio.maciel93@gmail.com>
2018-10-04 11:55:13 -0700

    Initial Commit

---
+++ flix_demo_03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix_demo_03
+//
+//  Created by Joseantonio Maciel on 10/4/18.
+//  Copyright © 2018 Joseantonio Maciel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
